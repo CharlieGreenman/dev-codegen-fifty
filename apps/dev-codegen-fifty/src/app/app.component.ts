e1d11d2dc3b9ed3b6efbc39b892df181c59d25c8
Charlie Greenman <charlie@razroo.com>
2023-09-05 00:44:17 -0400

    Create starter files for angular-16.2.0

---
+++ apps/dev-codegen-fifty/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dev-codegen-fifty-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'dev-codegen-fifty';
+}
